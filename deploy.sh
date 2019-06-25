19fb7e8ca6f42d45cf73f90e14e6520771fa4e97
Ryan <ryannitz@amazon.com>
2019-06-25 12:30:08 -0400

    Dev

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,23 @@
 
 rm -Rf lambda-update-sqs-queue-depth.zip main
 
-BUCKET_NAME=public-aws-serverless-repo
+BUCKET_PREFIX=pub-cfn-cust-res-pocs
 GOOS=linux go build main.go
+
+REGIONS=$(aws ec2 describe-regions --output text --query Regions[*].RegionName)
+
+# Currently, StackSets are not supported in these regions. This may change over time.
+REGIONS=${REGIONS//eu-north-1/}
+REGIONS=${REGIONS//ap-northeast-3/}
 
 zip lambda-update-sqs-queue-depth.zip ./main
 
-aws s3 cp lambda-update-sqs-queue-depth.zip s3://$BUCKET_NAME/lambda-update-sqs-queue-depth.zip
+for REGION in $REGIONS; do
+  aws s3 cp lambda-update-sqs-queue-depth.zip s3://$BUCKET_PREFIX-$REGION/lambda-update-sqs-queue-depth.zip --region $REGION
+  aws s3api put-object-tagging --region $REGION --bucket $BUCKET_PREFIX-$REGION --key lambda-update-sqs-queue-depth.zip --tagging 'TagSet={Key=public,Value=yes}'
+done
 
-aws s3api put-object-tagging --bucket $BUCKET_NAME --key lambda-update-sqs-queue-depth.zip --tagging 'TagSet={Key=public,Value=yes}'
+
+
+
+
